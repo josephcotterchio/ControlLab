9443a63cc4509f681c7767407df025d7262bd2f5
joecotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-08-31 16:36:57 -0400

    changes

---
+++ exercise-2.py
@@ -0,0 +1,16 @@
+# exercise-02 Length of Phrase
+
+# Write the code that:
+# 1. Prompts the user to enter a phrase:
+#      Please enter a word or phrase:
+# 2. Print the following message:
+#      - What you entered is xx characters long
+# 3. Return to step 1, unless the word 'quit' was entered.
+
+
+phrase = input("Please enter a word or phrase")
+print("what you entered is" + " " + str(len(phrase)) + " " + "characters long")
+while phrase != "quit":
+    phrase = input("Please enter a word/phrase or type quit to exit ")
+    print("what you entered is" + " " +
+          str(len(phrase)) + " " + "characters long")
---
+++ exercise-3.py
@@ -0,0 +1,21 @@
+# exercise-03 Calculate Dog Years
+
+# Write the code that:
+# 1. Prompts the user to enter a dog's age in human years like this:
+#      Input a dog's age in human years:
+# 2. Calculates the equivalent dog years, where:
+#      - The first two years count as 10 years each
+#      - Any remaining years count as 7 years each
+# 3. Prints the answer in the following format:
+#      The dog's age in dog years is xx
+
+# Hint:  Use the int() function to convert the string returned from input() into an integer
+
+num = int(input("Input a dog's age in human years: "))
+first_years = 10
+second_years = 7
+if num >= 2:
+    num2 = 20
+    print(num * first_years - num2)
+else:
+    print(num2 * second_years - num)
